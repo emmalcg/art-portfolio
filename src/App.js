176f19458a3c2a521d51ca48c24d7bffedf841a8
emma <emmalcgreen@gmail.com>
2021-03-27 22:28:13 -0400

    under construction

--- src/App.js
+++ src/App.js
@@ -4,17 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Emma Green</h1>
-      <nav>
-        <ul>
-          <li>fountains to somewhere</li>
-          <li>what makes a city good to live in?</li>
-          <li>searching everywhere for you</li>
-          <li>when you touch one thing, you have to touch all things</li>
-          <li>to produce (a mark)</li>
-          <li>info</li>
-          <li>CV</li>
-        </ul>
-      </nav>
+      <h2>under construction ~ check back soon</h2>
     </div>
   );
 }
